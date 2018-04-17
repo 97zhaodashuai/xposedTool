298dd4cc054bbba13c1853071e5c09d6e6cf2eba
zhaodsh <97shuai@163.com>
2018-04-17 11:43:40 +0800

    xposed module can action

---
+++ app/src/main/java/com/zhaodsh/xposedtool/XPMain.java
@@ -0,0 +1,14 @@
+package com.zhaodsh.xposedtool;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XPMain implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+
+        
+
+    }
+}
